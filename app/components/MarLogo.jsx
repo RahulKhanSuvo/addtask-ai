d82b10a765a6e5e750e2bee3e53fa4590301c268
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-02-17 16:20:58 +0600

    Update MarLogo component to increase spacing between logos

--- app/components/MarLogo.jsx
+++ app/components/MarLogo.jsx
@@ -5,58 +5,58 @@ export default function MarLogo() {
   return (
     <div className="py-5">
       <Marquee speed={50} gradient={false}>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Workday.svg" alt="Logo 1" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Google.svg" alt="Logo 2" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Salesforce.svg" alt="Logo 3" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Amazon.svg" alt="Logo 4" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Sony.svg" alt="Logo 4" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/intel.svg" alt="Logo 4" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Workday.svg" alt="Logo 1" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Google.svg" alt="Logo 2" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Salesforce.svg" alt="Logo 3" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Amazon.svg" alt="Logo 4" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Sony.svg" alt="Logo 4" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/intel.svg" alt="Logo 4" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Workday.svg" alt="Logo 1" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Google.svg" alt="Logo 2" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Salesforce.svg" alt="Logo 3" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Amazon.svg" alt="Logo 4" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/Sony.svg" alt="Logo 4" width={80} height={80} />
         </div>
-        <div className="mx-4">
+        <div className="mx-[50px]">
           <Image src="/intel.svg" alt="Logo 4" width={80} height={80} />
         </div>
       </Marquee>
